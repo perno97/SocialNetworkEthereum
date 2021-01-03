9b75d36934c433a3d8a792a53da00298507aea58
Fabio <fabio.perini97@gmail.com>
2021-01-03 12:01:37 +0100

    Using docs directory

--- truffle-config.js
+++ truffle-config.js
@@ -8,7 +8,7 @@ module.exports = {
       network_id: "*" // Match any network id
     }
   },
-  contracts_build_directory: "./src/contracts",
+  contracts_build_directory: "./docs/contracts",
   compilers: {
     solc: {
       version: "0.8.0",
