4f88f4ab12e8c640d3427ded9427317c4e40c1ea
Fabio <fabio.perini97@gmail.com>
2021-01-05 11:16:53 +0100

    Saving timestamp

--- docs/js/app.js
+++ docs/js/app.js
@@ -5,6 +5,7 @@ App = {
   isRegistered: false,
   ipfs: null,
 
+  //functions called on connect button click
   connect: function() {
     App.initWeb3();
   },
@@ -39,7 +40,7 @@ App = {
       App.contracts.User = TruffleContract(user);
       // Connect provider to interact with contract
       App.contracts.User.setProvider(App.web3Provider);
-    }).then(function(){
+    }).then(function() {
       $.getJSON("./contracts/AccountManager.json", function(accountManager) {
         // Instantiate a new truffle contract from the artifact
         App.contracts.AccountManager = TruffleContract(accountManager);
@@ -49,24 +50,28 @@ App = {
       });
     })
   },
+  //END functions called on connect button click
 
-  checkRegistration: function(){
-    App.contracts.AccountManager.deployed().then(function(instance){
-      App.accountManagerInstance=instance;
-      App.accountManagerInstance.isSenderRegistered().then(function(isRegistered){
+  //functions called on register button click
+  register: function() {
+    App.accountManagerInstance.register().then(function() {
+      App.checkRegistration();
+    });
+  },
+
+  checkRegistration: function() {
+    App.contracts.AccountManager.deployed().then(function(instance) {
+      App.accountManagerInstance = instance;
+      App.accountManagerInstance.isSenderRegistered().then(function(isRegistered) {
         App.isRegistered = isRegistered;
         App.render();
       });
     });
   },
+  //END functions called on register button click
 
-  register: function(){
-    App.accountManagerInstance.register().then(function(){
-      App.checkRegistration();
-    });
-  },
-
-  login: function(){
+  //function called on login button click
+  login: function() {
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
@@ -75,22 +80,24 @@ App = {
     });
 
     App.contracts.AccountManager.deployed().then(function(instance) {
-      App.accountManagerInstance=instance;
-      App.accountManagerInstance.getThisUserAddress().then(function(address){
+      App.accountManagerInstance = instance;
+      App.accountManagerInstance.getThisUserAddress().then(function(address) {
         App.userContractAddress = web3.toChecksumAddress(address);
         return App.contracts.User.at(App.userContractAddress);
-      }).then(function(instance){
+      }).then(function(instance) {
         App.userContractInstance = instance;
         App.render();
       });
     });
   },
 
+  //function called on reload button click or when UI needs to be updated
   render: function() {
     $("#accountAddress").html("Your account: " + App.account);
     $("#userContractAddress").html("Your user contract:" + App.userContractAddress);
 
-    if(App.web3Provider == null){
+    //update status and enable/disable buttons
+    if (App.web3Provider == null) {
       $("#isConnected").html("false");
       $("#isLogged").html("false");
       $("#isRegistered").html("false");
@@ -98,41 +105,39 @@ App = {
       $("#register").prop("disabled", true);
       $("#addPostDiv").hide();
       return;
-    }
-    else{
+    } else {
       $("#isConnected").html("true");
-      if(App.isRegistered && App.ipfs != null){
+      if (App.isRegistered && App.ipfs != null) {
         $("#register").prop("disabled", true);
         $("#login").prop("disabled", false);
         $("#isRegistered").html("true");
-      }
-      else {
+      } else {
         $("#register").prop("disabled", false);
         $("#login").prop("disabled", true);
         $("#isRegistered").html("false");
       }
     }
 
-    if(App.accountManagerInstance == null || App.userContractInstance == null) {
+    if (App.accountManagerInstance == null || App.userContractInstance == null) {
       $("#isLogged").html("false");
       return;
-    }
-    else{
+    } else {
       $("#isLogged").html("true");
       $("#addPostDiv").show();
     }
 
     //render posts
-    App.userContractInstance.getPostsCount().then(async function(count){
+    App.userContractInstance.getPostsCount().then(async function(count) {
       var postTemplate = "<br>";
-      for(var i=0;i<count;i++){
-        postTemplate = await App.userContractInstance.getPost(i).then(function(post){
+      for (var i = 0; i < count; i++) {
+        postTemplate = await App.userContractInstance.getPost(i).then(function(post) {
           const ipfsHash = post[1];
           var result = App.ipfs.cat(ipfsHash);
-          return result.next().then(function(r){
+          return result.next().then(function(r) {
             var string = new TextDecoder("utf-8").decode(r.value);
-            return "<tr><th>" + post[2] + "</th><td>" + post[0] + "</td><td>" + string + "</td></tr>" + postTemplate;
-          }).catch(function(error){
+            var timestamp = getTimestampFormatted(post[2]);
+            return "<tr><th>" + timestamp + "</th><td>" + post[0] + "</td><td>" + string + "</td></tr>" + postTemplate;
+          }).catch(function(error) {
             console.warn(error);
           });
         });
@@ -141,10 +146,10 @@ App = {
     });
 
     //render followings
-    App.userContractInstance.getFollowingCount().then(async function(count){
+    App.userContractInstance.getFollowingCount().then(async function(count) {
       var followingTemplate = "";
       var address;
-      for(var i=0;i<count;i++){
+      for (var i = 0; i < count; i++) {
         address = web3.toChecksumAddress(await App.userContractInstance.getFollowing(i));
         followingTemplate = '<tr><td><a href="" onclick="App.onUnfollow(\'' + address + '\'); return false;">' + address + "</a></td></tr>" + followingTemplate;
       }
@@ -152,15 +157,14 @@ App = {
     });
 
     //render users
-    App.accountManagerInstance.getUsersCount().then(async function(count){
+    App.accountManagerInstance.getUsersCount().then(async function(count) {
       var userAddress;
       var addressTemplate = "";
-      for(var i=0;i<count;i++){
+      for (var i = 0; i < count; i++) {
         userAddress = web3.toChecksumAddress(await App.accountManagerInstance.getUserAddress(i));
-        if(userAddress == App.userContractInstance.contract.address){
+        if (userAddress == App.userContractInstance.contract.address) {
           addressTemplate = "<tr><td>" + userAddress + " - (this)</td></tr>" + addressTemplate;
-        }
-        else {
+        } else {
           addressTemplate = '<tr><td><a href="" onclick="App.onFollow(\'' + userAddress + '\'); return false;">' + userAddress + "</a></td></tr>" + addressTemplate;
         }
       }
@@ -168,59 +172,57 @@ App = {
     });
   },
 
+  //function called on add post button click
   addPost: async function() {
-    if(App.userContractInstance == null) return;
+    if (App.userContractInstance == null) return;
     const newPostText = $("#newPostText").val();
     const contentToIpfs = $("#contentToIpfs").val();
 
     const result = await App.ipfs.add(contentToIpfs);
     newPostIpfsHash = result.cid.string;
-    var newPostTimestamp = getTimestampFormatted();
-    App.userContractInstance.addPost(newPostText,newPostIpfsHash,newPostTimestamp).then(function(){
+    var newPostTimestamp = "" + new Date().getTime();
+    App.userContractInstance.addPost(newPostText, newPostIpfsHash, newPostTimestamp).then(function() {
       App.render();
     });
   },
 
-  onFollow: function(address){
-    App.userContractInstance.addFollowing(address).then(function(){
+  //function called when clicking on user address link
+  onFollow: function(address) {
+    App.userContractInstance.addFollowing(address).then(function() {
       App.render();
     });
   },
 
-  onUnfollow: function(address){
-    App.userContractInstance.deleteFollowing(address).then(function(){
-      App.render();
-    });
-  },
-
-  onLike: function(postId){
-    App.contracts.SocialNetwork.deployed().then(function(instance) {
-      return instance.likePost(postId, {from: App.account });
-    }).then(function() {
+  //function called when clicking on following address link
+  onUnfollow: function(address) {
+    App.userContractInstance.deleteFollowing(address).then(function() {
       App.render();
     });
   }
 };
 
-function getTimestampFormatted(){
-  var today = new Date();
+function getTimestampFormatted(timestamp) {
+  var today = new Date(parseInt(timestamp));
   var dd = today.getDate();
-  var mm = today.getMonth()+1;
+  var mm = today.getMonth() + 1;
   var yyyy = today.getFullYear();
   var hour = today.getHours();
   var min = today.getMinutes();
   var sec = today.getSeconds();
-  if(dd<10) dd='0'+dd;
-  if(mm<10) mm='0'+mm;
-  if(hour<10) hour='0'+hour;
-  if(min<10) min='0'+min;
-  if(sec<10) sec='0'+sec;
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+  if (hour < 10) hour = '0' + hour;
+  if (min < 10) min = '0' + min;
+  if (sec < 10) sec = '0' + sec;
 
-  return mm+'/'+dd+'/'+yyyy + " - " + hour + ":" + min + ":" + sec;
+  return mm + '/' + dd + '/' + yyyy + " - " + hour + ":" + min + ":" + sec;
 }
 
+//initialize ipfs and update UI at startup (disabling buttons)
 $(document).ready(function() {
-  Ipfs.create({ repo: 'ipfs-' + Math.random() }).then(function(result){
+  Ipfs.create({
+    repo: 'ipfs-' + Math.random()
+  }).then(function(result) {
     App.ipfs = result;
   });
   App.render();
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,6 @@ var User = artifacts.require("./User.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(AccountManager);
+  //unused User deployment required for truffle-ganache to work
   deployer.deploy(User, "0x0000000000000000000000000000000000000000");
 };
